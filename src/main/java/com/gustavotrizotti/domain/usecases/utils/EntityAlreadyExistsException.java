cdcb8a8ff707e96aa747970fee963d87b6733e4e
Gustavo Trizotti <gustavotrizotti@newgesti.com.br>
2024-03-24 15:41:30 -0300

    Feat: add utils/validator classes to usecases

---
+++ src/main/java/com/gustavotrizotti/domain/usecases/utils/EntityAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.gustavotrizotti.domain.usecases.utils;
+
+public class EntityAlreadyExistsException extends RuntimeException {
+    public EntityAlreadyExistsException(String message) {
+        super(message);
+    }
+}
